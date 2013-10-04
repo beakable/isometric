a0bf8648595d3c16a642aefe2c40d8739b77e484
ihamilton@esri.com <ihamilton@esri.com>
2013-10-03 22:54:43 -0700

    Added basic liscense info

--- com/CanvasInput/CanvasInput.js
+++ com/CanvasInput/CanvasInput.js
@@ -1,4 +1,17 @@
-// Canvas inputs
+/*  This file is part of Iain Hamiltons Isometric HTML5 App.
+
+    Iain Hamiltons Isometric HTML5 App is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Iain Hamiltons Isometric HTML5 App is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Iain Hamiltons Isometric HTML5 App.  If not, see <http://www.gnu.org/licenses/>. */
 
 function CanvasInput(doc, canvas) {
 
