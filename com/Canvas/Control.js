2a4d8d1b90e025733d7bb0cf741607442d9a3ec0
ihamilton@esri.com <ihamilton@esri.com>
2013-10-31 22:23:37 -0700

    Simple browser support check

--- com/Canvas/Control.js
+++ com/Canvas/Control.js
@@ -30,14 +30,23 @@ canvas = (function() {
   }
 
   function _create(name, w, h, style) {
-    canvasElement = document.createElement('canvas');
-    canvasElement.id = name;
-    canvasElement.width = w || window.innerWidth;
-    canvasElement.height = h || window.innerHeight;
-    for (var s in style){
-      canvasElement.style[s] = style[s];
+    if (_supported()){
+      canvasElement = document.createElement('canvas');
+      canvasElement.id = name;
+      canvasElement.width = w || window.innerWidth;
+      canvasElement.height = h || window.innerHeight;
+      for (var s in style){
+        canvasElement.style[s] = style[s];
+      }
+      return document.body.appendChild(canvasElement).getContext('2d');
     }
-    return document.body.appendChild(canvasElement).getContext('2d');
+    else {
+      var noCanvas = document.createElement("div");
+      noCanvas.style.color = "#FFF";
+      noCanvas.style.textAlign = "center";
+      noCanvas.innerHTML = "Sorry, you need to use a more modern browser.";
+      return document.body.appendChild(noCanvas);
+    }
   }
 
   function _fullScreen() {
